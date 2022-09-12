409bcbb83708783f954092d74aa976f76c4cd10f
rasmus-kirk <mail@rasmuskirk.com>
2022-09-13 01:11:50 +0200

    laptop | 1054: fixing wanikani waybar

--- modules/desktop/sway/submodules/waybar/default.nix
+++ modules/desktop/sway/submodules/waybar/default.nix
@@ -29,7 +29,7 @@ let
 		else
 			class=reviews
 		fi
-		echo '{"text":"'$unread'  日","class":"'$class'"}'
+		echo '{"text":"'$unread'  復習","class":"'$class'"}'
 	'';
 
 	update-wanikani = pkgs.writeShellScript "update-wanikani" ''
